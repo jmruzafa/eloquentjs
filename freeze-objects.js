dddd67f48eaed4eab866bed530c965460192fd8e
Javier M. Ruzafa <javier.reyes@adidas.com>
2022-08-18 21:46:57 +0200

    Updating robots with routes (minimize movements)

---
+++ freeze-objects.js
@@ -0,0 +1,4 @@
+let object = Object.freeze({value:5});
+object.value = 10;
+console.log(object.value);
+
--- road-graph.js
+++ road-graph.js
@@ -14,7 +14,7 @@ const roads = [
   "Marketplace-Town Hall",
   "Shop-Town Hall",
 ];
-
+//graph function
 function buildGraph(edges) {
   let graph = Object.create(null);
   function addEdge(from, to) {
@@ -34,6 +34,7 @@ function buildGraph(edges) {
 }
 const roadGraph = buildGraph(roads);
 
+//building the village class and the robots
 class VillageState {
   constructor(place, parcels) {
     this.place = place;
@@ -54,9 +55,81 @@ class VillageState {
   }
 }
 
-let first = new VillageState("Post Office", [{place: "Post Office", address: "Alice's House"}]);
+/* let first = new VillageState("Post Office", [
+  { place: "Post Office", address: "Alice's House" },
+]);
 let next = first.move("Alice's House");
 console.log(next.place);
 console.log(next.parcels);
 
-console.log(first.place);
+console.log(first.place); */
+
+function runRobot(state, robot, memory) {
+  for (let turn = 0; ; turn++) {
+    if (state.parcels.length == 0) {
+      console.log(`Done in ${turn} turns`);
+      break;
+    }
+    let action = robot(state, memory);
+    state = state.move(action.direction);
+    memory = action.memory;
+    console.log(`Moved to ${action.direction}`);
+  }
+}
+function randomPick(array) {
+  let choice = Math.floor(Math.random() * array.length);
+  return array[choice];
+}
+function randomRobot(state) {
+  return { direction: randomPick(roadGraph[state.place]) };
+}
+
+//static method
+VillageState.random = function (parcelCount = 5) {
+  let parcels = [];
+  for (let i = 0; i < parcelCount; i++) {
+    let address = randomPick(Object.keys(roadGraph));
+    let place;
+    do {
+      place = randomPick(Object.keys(roadGraph));
+    } while (place == address);
+    parcels.push({ place, address });
+  }
+  return new VillageState("Post Office", parcels);
+};
+runRobot(VillageState.random(), randomRobot);
+
+//adding memory to the robots
+const mailRoute = [
+  "Alice's House",
+  "Cabin",
+  "Alice's House",
+  "Bob's House",
+  "Town Hall",
+  "Daria's House",
+  "Ernie's House",
+  "Grete's House",
+  "Shop",
+  "Grete's House",
+  "Farm",
+  "Marketplace",
+  "Post Office",
+];
+
+function routeRobot(state, memory) {
+  if (memory.length == 0) {
+    memory = mailRoute;
+  }
+  return { direction: memory[0], memory: memory.slice(1) };
+}
+function findRoute(graph, from, to) {
+  for (let i = 0; i < work.length; i++) {
+    let work = [{ at: from, route: [] }];
+    for (let place of graph[at]) {
+      if (place == to) return route.concat(place);
+      if (!work.some((w) => w.at == place)) {
+        work.push({ at: place, route: route.concat(place) });
+      }
+    }
+  }
+}
